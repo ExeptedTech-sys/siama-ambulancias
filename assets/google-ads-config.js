2369072cc9ac9eb1afc3594774e1c8c3b6be3569
ExeptedTech-sys <exeptedtech2205@gmail.com>
2026-09-07 01:45:31 -0600

    Add files via upload

---
+++ assets/google-ads-config.js
@@ -0,0 +1,6 @@
+// Reemplaza los valores cuando Google Ads te entregue tus identificadores.
+window.SIAMA_GOOGLE_ADS_ID = 'AW-XXXXXXXXX';
+window.SIAMA_GOOGLE_ADS_CONVERSIONS = {
+  call: '',
+  whatsapp: ''
+};
